4c9aec4a6fe95267f98030f44633b55208a11211
tharusha1004 <tharusha.astro@gmail.com>
2024-02-01 10:23:09 +0530

    add routing for each page inside app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List />} />
+        <Route path='/hotels/:id' element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
