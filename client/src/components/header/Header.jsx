0f100178d0c41a3a828d03c9c6859e4c4cfacdf6
tharusha1004 <tharusha.astro@gmail.com>
2024-02-02 12:17:15 +0530

    integrate calendar into search bar in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../header/Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBed, faCalendarDays, faCar, faPerson, faPlaceOfWorship, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { DateRange } from 'react-date-range'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns'
 
 const Header = () => {
+    const [openDate, setOpenDate] = useState(false)
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: 'selection'
+        },
+    ]);
+
     return (
         <div className='header'>
             <div className='headerContainer'>
@@ -37,7 +50,14 @@ const Header = () => {
                     </div>
                     <div className='headerSearchItem'>
                         <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-                        <span className='headerSearchText'>date to date</span>
+                        <span onClick={()=>setOpenDate(!openDate)} className='headerSearchText'>{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")} `}</span>
+                        {openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={(item) => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className='date'
+                        />}
                     </div>
                     <div className='headerSearchItem'>
                         <FontAwesomeIcon icon={faPerson} className='headerIcon' />
