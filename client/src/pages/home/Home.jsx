8273fb8cc9e0a6ed4ff97f4aebe544c24d1ebeb2
tharusha1004 <tharusha.astro@gmail.com>
2024-02-02 10:40:27 +0530

    Add header component into home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../home/Home.css'
 import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Header />
         </div>
     )
 }
