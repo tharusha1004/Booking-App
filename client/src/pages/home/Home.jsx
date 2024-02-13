f7c04c2d5ce675e3a33f66dca65f05495fe8a37f
tharusha1004 <tharusha.astro@gmail.com>
2024-02-13 10:49:34 +0530

    add FeaturedProperties Component into home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
 import PropertyList from '../../components/propertyList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 
 const Home = () => {
     return (
@@ -14,6 +15,8 @@ const Home = () => {
                 <Featured />
                 <h1 className="homeTitle">Browse by Property Type</h1>
                 <PropertyList />
+                <h1 className="homeTitle">Homes Guests Love</h1>
+                <FeaturedProperties />
             </div>
         </div>
     )
